f489e04cbe7da612cc127324cfdadc225c5b0c51
JakubBlacha <114864986+JakubBlacha@users.noreply.github.com>
2022-10-29 21:16:06 +0200

    Update test_views.py

--- test/test_views.py
+++ test/test_views.py
@@ -15,4 +15,6 @@ class FlaskrTestCase(unittest.TestCase):
 
     def test_msg_with_output(self):
         rv = self.app.get('/?output=json')
-        self.assertEqual(b'{"imie":"Julia", "msg":"Hello World!"}', rv.data)
+        self.assertEqual(b'{"imie":"Jakub", "msg":"Hello World!"}', rv.data)
+
+      
